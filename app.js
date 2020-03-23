2033910d3e8e51f118e65ec429e12d8d4a8b52e6
hanialazreg <lazreghania@gmail.com>
2020-03-23 23:13:33 +0100

    work with mongoose after understand mongoodb

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoConnect = require("./backend/config/database");
+const dbconnection = require("./backend/config/database");
 
 const app = express();
 
@@ -8,11 +8,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   res.send("just testing  ");
 });
+let con = dbconnection();
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
-});
-mongoConnect(client => {
-  console.log(client);
-  app.listen(8080);
 });
--- backend/config/database.js
+++ backend/config/database.js
@@ -1,17 +1,18 @@
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+const mongoose = require("mongoose");
 
-const mongoConnect = callback => {
-  MongoClient.connect(
-    "mongodb+srv://hania:HHHAAA222TTT@trainning-ya2n4.mongodb.net/test?retryWrites=true&w=majority"
-  )
-    .then(client => {
-      console.log("you are connected know");
-      //client object gives us acces to database
-      callback(client);
-    })
-    .catch(err => {
-      console.log("the errorrrrr", err);
-    });
+const connectDB = async () => {
+  try {
+    return (conn = mongoose.connect(
+      "mongodb+srv://hania:HHHAAA222TTT@trainning-ya2n4.mongodb.net/test?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    ));
+    console.log(`Mongo is Connected`);
+  } catch (error) {
+    console.log(`Error ${e.message}`);
+  }
 };
-module.exports = mongoConnect;
+
+module.exports = connectDB;
